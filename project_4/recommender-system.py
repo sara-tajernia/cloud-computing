113f37233ae9ac95015fe7c185ac4dcfc50e6bc6
sara_tjn <tajernia.sara@gmail.com>
2023-01-25 04:52:49 +0800

    build matrix and store data

--- project_4/recommender-system.py
+++ project_4/recommender-system.py
@@ -1,3 +1,4 @@
+###### Step 1: Import Python Libraries
 # Data processing
 import pandas as pd
 import numpy as np
@@ -31,3 +32,52 @@ matrix = merged_data.pivot_table(index='game_id', columns='user_id', values='rat
 # Normalize user-item matrix
 matrix_norm = matrix.subtract(matrix.mean(axis=1), axis = 'rows')
 matrix_norm.head()
+###### Step 6: Identify Similar Users
+# User similarity matrix using Pearson correlation
+user_similarity = matrix_norm.T.corr()
+user_similarity.head()
+# User similarity matrix using cosine similarity
+user_similarity_cosine = cosine_similarity(matrix_norm.fillna(0))
+user_similarity_cosine
+# Number of similar users
+n = 10
+# User similarity threashold
+user_similarity_threshold = 0.3
+# Get top n similar users
+similar_users = user_similarity[user_similarity[picked_userid]>user_similarity_threshold][picked_userid].sort_values(ascending=False)[:n]
+# Print out top n similar users
+print(f'The similar users for user {picked_userid} are', similar_users)
+###### Step 7: Narrow Down Item Pool
+# Remove movies that have been watched
+picked_userid_watched = matrix_norm[matrix_norm.index == picked_userid].dropna(axis=1, how='all')
+picked_userid_watched
+# Movies that similar users watched. Remove movies that none of the similar users have watched
+similar_user_movies = matrix_norm[matrix_norm.index.isin(similar_users.index)].dropna(axis=1, how='all')
+similar_user_movies
+# Remove the watched movie from the movie list
+similar_user_movies.drop(picked_userid_watched.columns,axis=1, inplace=True, errors='ignore')
+# Take a look at the data
+similar_user_movies
+###### Step 8: Recommend Items
+# A dictionary to store item scores
+item_score = {}
+# Loop through items
+for i in similar_user_movies.columns:
+  # Get the ratings for movie i
+  movie_rating = similar_user_movies[i]
+  # Create a variable to store the score
+  total = 0
+  # Create a variable to store the number of scores
+  count = 0
+  # Loop through similar users
+  for u in similar_users.index:
+    # If the movie has rating
+    if pd.isna(movie_rating[u]) == False:
+      # Score is the sum of user similarity score multiply by the movie rating
+      score = similar_users[u] * movie_rating[u]
+      # Add the score to the total score for the movie so far
+      total += score
+      # Add 1 to the count
+      count +=1
+  # Get the average score for the item
+  item_score[i] = total / count
